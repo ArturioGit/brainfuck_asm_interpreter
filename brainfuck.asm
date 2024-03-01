75dfaed0c6bc796141917e801913a82d8800bcb5
ArturioGit <klyuchka.artur@gmail.com>
2024-03-01 17:24:29 +0200

    add closing file

--- brainfuck.asm
+++ brainfuck.asm
@@ -1,5 +1,14 @@
 .model tiny
 
+
+; constatnts 
+MAX_SIZE EQU 10000
+
+.data?
+; uninitialized data
+filename db 256d dup(?)
+code db MAX_SIZE dup(?)
+
 .code
 org 100h
 
@@ -10,23 +19,24 @@ init:
 
     ; set the value of the register si to correctly read the value of the argument (file name)
     mov si, 82h
-    lea di, file_name 
+    lea di, filename 
     cld
 
-copy_file_name:
+copy_filename:
     ; read the argument byte by byte and write it into memory using movsb
     movsb
-    mov al, byte [si]
-    cmp al, ' '
+    mov dl, byte [si]
+    cmp dl, ' '
     je open_file
-    cmp al, 0
+    cmp dl, 0
     je open_file
-    jmp copy_file_name
+    jmp copy_filename
 
 open_file:
     ; just opening the file
+    mov ds, ax
     xor ax, ax
-    lea dx, file_name
+    lea dx, filename
     mov ah, 03dh
     int 21h
     
@@ -35,22 +45,18 @@ read_file:
     ; call read file function 3fh, put the contents of the file into memory (code)
     mov bx, ax
     mov ah, 03fh
-    mov cx, [max_size]
+    mov cx, MAX_SIZE
     mov dx, offset code
+    int 21h
+
+close_file:
+    ; just closing file
+    mov ah, 3Eh
     int 21h
 
 exit:
     ; exit instructions
     mov ax, 4C00h
     int 21h
-
-.data
-; initialized data
-max_size dw 10000d
-
-.data?
-; uninitialized data
-file_name db 256d dup(?)
-code db 10000d dup(?)
 
 end init
